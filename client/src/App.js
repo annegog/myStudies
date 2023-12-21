aa59737093d767a627e747fcae8aea6e9a793a92
annegog <annegog>
2023-12-21 23:26:01 +0200

    new

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+// App.js
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { auth, firebaseConfig } from './config/firebase';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+import Register from './component/Register';
+import Login from './component/Login';
+import Courses from './component/Courses';
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login db={db} />} />
+          <Route path="register" element={<Register db={db} />} />
+          <Route path="courses" element={<Courses db={db} />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/component/auth.js
@@ -0,0 +1,8 @@
+export const Auth = ()=> {
+    return (
+        <div>
+            <input placeholder="username"/>
+            <input placeholder="password"/>
+        </div>
+    )
+}

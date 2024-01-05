ee84ee881e2931a6ba88317d25902260eb280405
annegog <annegog>
2024-01-05 11:57:11 +0200

    navbar changes

--- myStudies/src/components/Navbar_students.jsx
+++ myStudies/src/components/Navbar_students.jsx
@@ -22,34 +22,39 @@ const Navbar_students = () => {
           </div>
         </div>
 
-        <div className="hidden items-end md:justify-items-end md:flex gap-5 font-medium p-1 text-lg">
-          <Link
-            to="/student"
-            spy={true}
-            smooth={true}
-            duration={500}
-            className="hover:text-[#3f6591] transition-all cursor-pointer"
-          >
-            Αρχική Σελίδα
-          </Link>
-          <Link
-            to="/student/profile"
-            spy={true}
-            smooth={true}
-            duration={500}
-            className="hover:text-[#3f6591] transition-all cursor-pointer"
-          >
-            username
-          </Link>
-        </div>
-
-        <div className="flex items-end md:justify-items-end gap-5">
-          <button
-            type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Αποσύνδεση
-          </button>
+        <div className="hidden md:justify-items-end md:flex gap-5 font-medium p-1 text-center my-auto text-lg flex-col items-stretch w-[48%] ml-5 max-md:w-full max-md:ml-0">
+          <div className="flex items-stretch justify-between gap-5 my-auto max-md:max-w-full max-md:flex-wrap max-md:mt-10">
+            <div className="text-black text-center text-lg font-medium my-auto">
+              <Link
+                to="/student"
+                spy={true}
+                smooth={true}
+                duration={500}
+                className="hover:text-[#3f6591] transition-all cursor-pointer"
+              >
+                Αρχική Σελίδα
+              </Link>
+            </div>
+            <div className="text-black text-center text-lg font-medium my-auto">
+              <Link
+                to="/student/profile"
+                spy={true}
+                smooth={true}
+                duration={500}
+                className="hover:text-[#3f6591] transition-all cursor-pointer"
+              >
+                username
+              </Link>
+            </div>
+            <div className="flex items-end md:justify-items-end gap-5">
+              <button
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Αποσύνδεση
+              </button>
+            </div>
+          </div>
         </div>
 
         <div className="flex md:hidden" onClick={handleChange}>
@@ -88,3 +93,51 @@ const Navbar_students = () => {
 };
 
 export default Navbar_students;
+
+{
+  /* <div className="bg-zinc-300 z-[1] w-full pl-1.5 pr-10 pb-3 rounded-none max-md:max-w-full max-md:pr-5"> */
+}
+// <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+//           <div className="flex flex-col items-stretch w-[52%] max-md:w-full max-md:ml-0">
+//             <div className="grow max-md:max-w-full max-md:mt-10">
+//               <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+//                 <div className="flex flex-col items-stretch w-[32%] max-md:w-full max-md:ml-0">
+//                   <div className="flex grow flex-col items-center max-md:mt-10">
+//                     <img
+//                       loading="lazy"
+//                       srcSet="..."
+//                       className="aspect-[0.66] object-contain object-center w-[71px] overflow-hidden max-w-full"
+//                     />
+//                     <div className="text-black text-center text-xs font-medium self-stretch">
+//                       ΕΘΝΙΚΟΝ & ΚΑΠΟΔΙΣΤΡΙΑΚΟΝΠΑΝΕΠΙΣΤΗΜΙΟ ΑΘΗΝΩΝ
+//                     </div>
+//                   </div>
+//                 </div>
+//                 <div className="flex flex-col items-stretch w-[68%] ml-5 max-md:w-full max-md:ml-0">
+//                   <div className="flex flex-col items-stretch my-auto max-md:mt-10">
+//                     <div className="justify-center text-black text-4xl font-light">
+//                       My Studies
+//                     </div>
+//                     <div className="justify-center text-black text-center text-xl font-light whitespace-nowrap mt-1.5">
+//                       Γραμματείες Πανεπιστημίου Αθηνών
+//                     </div>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="flex flex-col items-stretch w-[48%] ml-5 max-md:w-full max-md:ml-0">
+//             <div className="flex items-stretch justify-between gap-5 my-auto max-md:max-w-full max-md:flex-wrap max-md:mt-10">
+//               <div className="text-black text-center text-lg font-medium my-auto">
+//                 ΑΡΧΙΚΗ ΣΕΛΙΔΑ
+//               </div>
+//               <div className="text-black text-center text-lg font-medium my-auto">
+//                 sdi202400001
+//               </div>
+//               <div className="justify-center text-white text-center text-xl font-medium whitespace-nowrap bg-sky-900 grow items-stretch pt-5 pb-3 px-7 rounded-xl max-md:px-5">
+//                 Αποσύνδεση
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>

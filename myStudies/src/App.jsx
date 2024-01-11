45f259559db5d2a4352c6f0060d9e06b84b400eb
ChristosPapastergiou <christospapastergiou.cs@gmail.com>
2024-01-11 22:15:30 +0200

    christos

--- myStudies/src/App.jsx
+++ myStudies/src/App.jsx
@@ -6,12 +6,14 @@ import Courses from "./pages/CoursesPage";
 import Profile from "./pages/ProfilePage";
 import MainPageS from "./pages/MainPage_students";
 import Grades from "./pages/GradesPage";
-import Certifications from "./pages/Certifications/Main";
-import DeclarationsStart from "./pages/Declarations/DeclarationPage_students";
-import DeclarationVerification from "./pages/Declarations/VerificationPage_students";
+
+import Certifications from "./pages/Certifications/Certifications";
 import CertificationsStatus from "./pages/Certifications/Status";
 import CertificationsRequest from "./pages/Certifications/Request";
 import CertificationsHistory from "./pages/Certifications/History";
+
+import DeclarationsStart from "./pages/Declarations/DeclarationPage_students";
+import DeclarationVerification from "./pages/Declarations/VerificationPage_students";
 
 axios.defaults.baseURL = 'http://localhost:4000';
 axios.defaults.withCredentials = true;
@@ -19,14 +21,26 @@ axios.defaults.withCredentials = true;
 const App = () => {
     return (
         <Routes>
+
+            {/* General */}
+
             <Route path="/" element={<LoginPage />} />
             <Route path="/login" element={<LoginPage />} />
+
+            {/* Student Main */}
+
             <Route path="/student" element={<MainPageS />} />
             <Route path="/student/grades" element={<Grades />} />
             <Route path="/student/courses" element={<Courses />} />
             <Route path="/student/profile" element={<Profile />} />
+
+            {/* Declarations */}
+            
             <Route path="/student/declarations/start" element={<DeclarationsStart />} />
             <Route path="/student/declarations/verification" element={<DeclarationVerification />} />
+
+            {/* Certificates */}
+
             <Route path="/student/certifications" element={<Certifications />} />
             <Route path="/student/certifications/status" element={<CertificationsStatus />} />
             <Route path="/student/certifications/request" element={<CertificationsRequest />} />

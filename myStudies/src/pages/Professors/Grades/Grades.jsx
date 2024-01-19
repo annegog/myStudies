1ac0c18f4a100bda4685164319bd1cdf5058406a
ChristosPapastergiou <christospapastergiou.cs@gmail.com>
2024-01-19 16:13:22 +0200

    prof

--- myStudies/src/pages/Professors/Grades/Grades.jsx
+++ myStudies/src/pages/Professors/Grades/Grades.jsx
@@ -18,40 +18,40 @@ const Grades = () => {
         navigate("/professor/show-grades")
     };
 
-    const [activeCourse, setActiveCourse] = useState(false);
+    const [activeCourses, setActiveCourses] = useState({});
 
     const toggleCourse = (course) => {
-        setActiveCourse(activeCourse === course ? null : course);
+        setActiveCourses({ ...activeCourses, [course]: !activeCourses[course] });
     };
-
 
     const courses = [
         "Εισαγωγή στον Προγραμματισμό",
         "Αντικειμενοστραφής Προγραμματισμός", 
-        "Αρχές Γλωσσών Προγραμματισμού"
+        "Αρχές Γλωσσών Προγραμματισμού",
     ]
 
     return (
         <div>
             <Navbar/>
             <NavBarOptions userType={"professor"}/>
-            <main className="Professor Main">
+            <main className="Professor Main">                
+                <h2 className="text-center text-3xl font-thin justify-center mt-10 mb-10"> Τα μαθήματα μου </h2>
                 {courses.map(courses => (
-                    <div style={{ marginTop: "2rem" }} className="bg-zinc-300 bg-opacity-50 flex flex-col  px-40 py-12 rounded-lg max-md:px-5">
-                        <div className="flex flex-row cursor-pointer text-left w-full text-lg py-2 focus:outline-none" onClick={() => toggleCourse(!activeCourse)}>
-                            <span>{activeCourse ? "▲" : "▼"}</span>
+                    <div className="flex flex-col bg-gray-300 bg-opacity-75 px-40 py-10 mt-5 mb-5 rounded-3xl max-md:px-5">
+                        <div className="flex flex-row text-left w-full text-lg py-2 cursor-pointer focus:outline-none" onClick={() => toggleCourse(courses)}>
+                            <span> {activeCourses[courses] ? "▲" : "▼"} </span>
                             <h> {courses} </h>
                         </div>
-                        {activeCourse && (
+
+                        {activeCourses[courses] && (
                             <div className="Options">
-                                <button  onClick={handleCreationGrades} className="bg-blue-500 text-black px-4 py-2 mr-10 rounded-3xl hover:bg-blue-600"> Δημιουργία Βαθμολογίου </button>
-                                <button  onClick={handleCreationGrades} className="bg-green-500 text-black px-4 py-2 mt-10 rounded-3xl hover:bg-blue-600"> Προβολή Βαθμολογίου </button>
+                                <button onClick={handleCreationGrades} className="bg-blue-500 text-black font-medium px-4 py-2 mt-2 mr-4 rounded-3xl hover:bg-blue-600"> Δημιουργία Βαθμολογίου </button>
+                                <button onClick={handleShowGrades} className="bg-green-500 text-black font-medium px-4 py-2 mt-2 mr-4 rounded-3xl hover:bg-green-600"> Προβολή Βαθμολογίου </button>
                             </div>
                         )}
                     </div>
                 ))}
             </main>
-
             <Footer/>
         </div>
     );
--- myStudies/src/pages/Professors/Main/Main.jsx
+++ myStudies/src/pages/Professors/Main/Main.jsx
@@ -14,7 +14,8 @@ const Main = () => {
         navigate("/professor/grades");
     };
 
-    const lessons = [
+    // Test Courses
+    const courses = [
         "Εισαγωγή στον Προγραμματισμό",
         "Αντικειμενοστραφής Προγραμματισμός", 
         "Αρχές Γλωσσών Προγραμματισμού"
@@ -25,28 +26,15 @@ const Main = () => {
             <Navbar/>
             <NavBarOptions userType={"professor"}/>
             <main className="Professor Main">
-                <div style={{ marginTop: "4rem" }} className="bg-zinc-300 bg-opacity-50 flex flex-col  px-40 py-12 rounded-lg max-md:px-5">
-                    <h2 className="text-2xl font-thin justify-center text-center mb-1"> Τα μαθήματα μου </h2>
+                <div style={{ marginTop: "4rem" }} className="bg-gray-300 bg-opacity-50 flex flex-col px-40 py-12 rounded-lg max-md:px-5">
+                    <h2 className="text-2xl font-thin justify-center text-center mb-10 underline"> Τα μαθήματα μου </h2>
 
-                    {lessons.map(lessons => (
+                    {courses.map(lessons => (
                         <div key={lessons}>
-                            <button className="flex flex-row text-left w-full text-lg py-2 focus:outline-none"
-                                onClick={() => handleClick()}
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth="1"
-                                    stroke="currentColor"
-                                    className="w-6 h-7"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                                    />
-                                    </svg>
+                            <button className="flex flex-row text-left w-full text-lg py-2 focus:outline-none" onClick={() => handleClick()}>
+                                <svg fill="none" viewBox="0 0 24 24" strokeWidth="1" stroke="currentColor" className="w-6 h-7">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                                </svg>
                                 <p> {lessons} </p>
                             </button>
                         </div>

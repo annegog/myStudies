0832982feb3d0e327e62a491c9b73113b85303cb
Anna Gogoula <gogoulaanna@gmail.com>
2024-01-22 14:05:58 +0200

    little changew on front

--- myStudies/src/pages/Professors/Grades/Grades.jsx
+++ myStudies/src/pages/Professors/Grades/Grades.jsx
@@ -35,22 +35,23 @@ const Grades = () => {
             <Navbar/>
             <NavBarOptions userType={"professor"}/>
             <main className="Professor Main">                
-                <h2 className="text-center text-3xl font-thin justify-center mt-10 mb-10"> Τα μαθήματα μου </h2>
-                {courses.map(courses => (
-                    <div className="flex flex-col bg-gray-300 bg-opacity-75 px-40 py-10 mt-5 mb-5 m-20 rounded-3xl max-md:px-5">
-                        <div className="flex flex-row text-left w-full text-lg py-2 cursor-pointer focus:outline-none" onClick={() => toggleCourse(courses)}>
-                            <span> {activeCourses[courses] ? "▲" : "▼"} </span>
-                            <h> {courses} </h>
-                        </div>
-
-                        {activeCourses[courses] && (
-                            <div className="Options">
-                                <button onClick={handleCreationGrades} className="bg-blue-500 text-black font-medium px-4 py-2 mt-2 mr-4 rounded-3xl hover:bg-blue-600"> Δημιουργία Βαθμολογίου </button>
-                                <button onClick={handleShowGrades} className="bg-green-500 text-black font-medium px-4 py-2 mt-2 mr-4 rounded-3xl hover:bg-green-600"> Προβολή Βαθμολογίου </button>
+                <div className="mt-10 justify-center items-center md:justify-items-center gap-5 px-6 lg:px-16 xl:px-32 bg-amber-50">
+                    <h2 className="text-center text-3xl font-thin justify-center mt-10 mb-10"> Τα μαθήματα μου </h2>
+                    {courses.map(courses => (
+                        <div className="bg-slate-200 p-2 rounded-lg mt-8 space-y-4">
+                            <div className="flex flex-row text-left w-full text-lg py-2 cursor-pointer focus:outline-none" onClick={() => toggleCourse(courses)}>
+                                <span> {activeCourses[courses] ? "▲" : "▼"} </span>
+                                    <h> {courses} </h>
                             </div>
-                        )}
-                    </div>
-                ))}
+                            {activeCourses[courses] && (
+                            <div className="Options">
+                                <button onClick={handleCreationGrades} className="bg-blue-500 text-black font-medium px-4 py-2 mt-1 mr-4 rounded-3xl hover:bg-blue-600"> Δημιουργία Βαθμολογίου </button>
+                                <button onClick={handleShowGrades} className="bg-green-500 text-black font-medium px-4 py-2 mt-1 mr-4 rounded-3xl hover:bg-green-600"> Προβολή Βαθμολογίου </button>
+                            </div>
+                            )}
+                        </div>
+                    ))}
+                </div>
             </main>
             <Footer/>
         </div>
--- myStudies/src/pages/Professors/Main/Main.jsx
+++ myStudies/src/pages/Professors/Main/Main.jsx
@@ -25,12 +25,11 @@ const Main = () => {
         <div>
             <Navbar/>
             <NavBarOptions userType={"professor"}/>
-            <main className="Professor Main">
-                <div className="bg-gray-300 bg-opacity-50 flex flex-col px-40 py-12 mx-20 my-20 rounded-3xl max-md:px-5">
-                    <h2 className="text-2xl font-thin justify-center text-center mb-10 underline"> Τα μαθήματα μου </h2>
-
+            <main className="Professor Main">                    
+                <div className="mt-10 justify-center items-center md:justify-items-center gap-5 px-6 lg:px-16 xl:px-32 bg-amber-50">
+                    <h2 className="text-2xl font-thin justify-center text-center mb-1"> Τα μαθήματα μου </h2>
                     {courses.map(lessons => (
-                        <div key={lessons}>
+                        <div key={lessons} className="bg-slate-200 p-2 rounded-lg mt-8 space-y-4">
                             <button className="flex flex-row text-left w-full text-lg py-2 focus:outline-none" onClick={() => handleClick()}>
                                 <svg fill="none" viewBox="0 0 24 24" strokeWidth="1" stroke="currentColor" className="w-6 h-7">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>

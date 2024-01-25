f305ceb381960ace1341133e6357b3d897ff84df
ChristosPapastergiou <christospapastergiou.cs@gmail.com>
2024-01-25 23:27:50 +0200

    si

--- myStudies/src/pages/Students/Home/Home.jsx
+++ myStudies/src/pages/Students/Home/Home.jsx
@@ -10,6 +10,18 @@ import Navbar from "../../../components/Common/Navbar";
 import Footer from "../../../components/Common/Footer";
 import NavBarOptions from "../../../components/Common/NavBarOptions";
 
+const Info = ({ infoString, infoDetails }) => {
+    return (
+        <div className="text-center bg-gray-50 rounded-lg w-full p-10 shadow-xl hover:shadow-2xl">
+            <p className="text-xl font-bold m-2"> {infoString} </p>
+            <div className="grid grid-cols-1 border-gray-200 border-t">
+                <div className="flex justify-between items-center pt-5"/>
+                <p className="text-lg"> {infoDetails} </p>
+            </div>
+        </div>
+    );
+};
+
 const MainPage = () => {
     const navigate = useNavigate();
 
@@ -64,28 +76,26 @@ const MainPage = () => {
         </nav>
 
         <div className="px-4 lg:px-16 xl:px-32">
-            {declarationInfo.open && !declarationInfo.declaration && (
-                <div className="justify-center items-center text-center">
-                    <h2 className="text-xl font-bold text-red-800 mt-2"> Έχουν ανοίξει οι δηλώσεις μαθημάτων. <br /> Όλοι οι φοιτητές καλούνται να πραγματοποιήσουν την δήλωση τους εως και τις {" "}
+            {declarationInfo.open && !declarationInfo.declaration ? (
+                <div className="text-center">
+                    <h2 className="text-xl font-bold text-red-800"> Έχουν ανοίξει οι δηλώσεις μαθημάτων. <br /> Όλοι οι φοιτητές καλούνται να πραγματοποιήσουν την δήλωση τους εως και τις {" "}
                         {formatDate(declarationInfo.end_date)}.
                     </h2>
 
-                    <div className="mt-2 md:justify-items-center gap-5">
-                        <button type="button" className="text-white bg-blue-900 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-xl text-lg px-6 py-2 text-center shadow-md hover:shadow-2xl"
+                    <div className="mt-2">
+                        <button type="button" className="text-center text-lg text-white font-medium bg-blue-900 hover:bg-blue-700 rounded-xl px-6 py-2 shadow-md hover:shadow-2xl"
                             onClick={Declare}
                         > Δήλωση Μαθημάτων
                         </button>
                     </div>
                 </div>
-            )}
-
-            {declarationInfo.open && declarationInfo.declaration && (
-                <div className="text-center justify-center items-center">
+            ) : declarationInfo.open && declarationInfo.declaration && (
+                <div className="text-center">
                     <h2 className="text-xl font-bold text-green-500"> Έχει πραγματοποιηθεί Δήλωση Μαθημάτων στις {formatDate(declarationInfo.last_decl)}, μπορεί να γίνει τροποποίηση της. </h2>
                     <h2 className="text-xl font-bold text-red-800"> Η γραμματεία θα λάβει υπόψη της μόνο την τελευταία Δήλωση. </h2>
 
                     <div className="mt-3 md:justify-items-center">
-                        <button type="button" className="text-white bg-blue-900 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-xl text-lg px-6 py-2 text-center shadow-md hover:shadow-2xl"
+                        <button type="button" className="text-center text-lg text-white font-medium bg-blue-900 hover:bg-blue-700 rounded-xl px-6 py-2 shadow-md hover:shadow-2xl"
                             onClick={Modification}
                         > Τροποποίηση Δήλωσης Μαθημάτων
                         </button>
@@ -98,22 +108,8 @@ const MainPage = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-5 p-5 md:px-16 xl:px-32">
                 <Donut ects={user.ects} />
                 <div className="flex flex-col justify-center items-center space-y-20">
-                    <div className="p-10 bg-gray-50 rounded-lg text-center w-full shadow-xl hover:shadow-2xl">
-                        <p className="text-xl font-bold leading-none p-1 m-2"> Μέσος Όρος </p>
-
-                        <div className="grid grid-cols-1 items-center border-gray-200 border-t justify-between">
-                            <div className="flex justify-between items-center pt-5"/>
-                            <p className="text-lg"> 6.48 </p>
-                        </div>
-                    </div>
-
-                    <div className="p-10 bg-gray-50 rounded-lg text-center w-full shadow-xl hover:shadow-2xl">
-                        <p className="text-xl font-bold leading-none p-1 m-2"> Περασμένα Μαθήματα </p>
-                        <div className="grid grid-cols-1 items-center border-gray-200 border-t justify-between">
-                            <div className="flex justify-between items-center pt-5"/>
-                            <p className="text-lg"> {user.s_courses} </p>
-                        </div>
-                    </div>
+                    <Info infoString={"Μέσος Όρος"} infoDetails={10} />
+                    <Info infoString={"Περασμένα Μαθήματα"} infoDetails={user.s_courses} />
                 </div>
             </div>
         </div>

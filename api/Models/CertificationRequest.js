643c0100676748f6a8d3273d5089873706fa6d8c
ChristosPapastergiou <christospapastergiou.cs@gmail.com>
2024-01-26 16:34:07 +0200

    d

--- api/Models/CertificationRequest.js
+++ api/Models/CertificationRequest.js
@@ -2,27 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const CertificationRequestSchema = new Schema({
-    certificationType: {
-        type: String,
-        required: true,
-        enum: ['Φοιτητικής Ιδιότητας', 'Φορολογικής Χρήσης', 'Αναλυτική βαθμολογία με προβιβάσιμους βαθμούς', 'Στρατολογική χρήση (Συνοπτικό)', 'Στρατολογική χρήση (Αναλυτικό)']
-    },
-    numberOfCopies: {
-        type: Number,
-        required: true,
-        min: 1
-    },
-    requestDate: {
-        type: Date,
-        required: true
-    },
-    // Assuming you have user IDs, you may want to link the request to a specific user
-    studentId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User', // or whatever your user model is called
-        required: true
-    }
-    // You can add other fields as necessary
+    certificationType: { type: String, required: true, enum: ['Φοιτητικής Ιδιότητας', 'Φορολογικής Χρήσης', 'Αναλυτική βαθμολογία με προβιβάσιμους βαθμούς', 'Στρατολογική χρήση (Συνοπτικό)', 'Στρατολογική χρήση (Αναλυτικό)'] },
+    studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    numberOfCopies: { type: Number, required: true, min: 1 },
+    requestDate: { type: Date, required: true }
 });
 
 const CertificationRequest = mongoose.model('CertificationRequest', CertificationRequestSchema);
